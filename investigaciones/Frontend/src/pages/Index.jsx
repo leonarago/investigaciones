2ce999164f499761d98fd921b96d2bf15371b369
Mauricio <mauriio.salazar721@gmail.com>
2021-12-21 00:49:57 -0500

    dev ops

--- investigaciones/Frontend/src/pages/Index.jsx
+++ investigaciones/Frontend/src/pages/Index.jsx
@@ -1,10 +1,14 @@
+import PrivateRoute from 'components/PrivateRoute';
 import Spinner from 'components/spinner';
+import PrivateLayout from 'layouts/PrivateLayout';
 import React from 'react';
+import Register from './auth/register';
+import NuevoProyecto from './proyectos/NuevoProyecto';
 
 const Index = () => {
   return (
     <div>
-      <div className='bg-blue-100 h-96'><Spinner/> </div>
+      <div className='bg-blue-100 h-96'><NuevoProyecto/> </div>
       <div className='bg-blue-200 h-96'><Spinner/> </div>
       <div className='bg-blue-300 h-96'><Spinner/> </div>
       <div className='bg-blue-400 h-96'><Spinner/> </div>
